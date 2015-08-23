c7492d02cd4e73cd265de1b4b5b8ed18a804ae7c
Wlad Dicario <wlad@2v-prod.com>
2015-08-23 18:06:32 +0200

    Update

--- ControlFlow Early Exit/Controllers/ControlFlowTableViewController.swift
+++ ControlFlow Early Exit/Controllers/ControlFlowTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ControlFlowTableViewController: UITableViewController {
 
-    // data for the table
+    // data for the table.
     dynamic var dataArray = [Person]()
     
     private enum DataLayer: Int {
